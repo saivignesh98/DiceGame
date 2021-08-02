0f4f846f4a41757533c2d74db93d667958760402
Sai Vignesh <dragoonvignesh1998@gmail.com>
2021-08-02 13:03:33 +0530

    Added hold button functionality

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 'use strict';
 
-//Set player scores as 0 initially
+//select elements
 const scorePlayer0 = document.getElementById('score--0');
 const scorePlayer1 = document.getElementById('score--1');
-scorePlayer0.textContent = 0;
-scorePlayer1.textContent = 0;
 const currentScore0 = document.getElementById('current--0');
 const currentScore1 = document.getElementById('current--1');
-
 const player0Element = document.querySelector('.player--0');
 const player1Element = document.querySelector('.player--1');
+const diceElement = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Set player scores as 0 initially
+scorePlayer0.textContent = 0;
+scorePlayer1.textContent = 0;
 
 //Hide dice element initially
-const diceElement = document.querySelector('.dice');
 diceElement.style.visibility = 'hidden';
 
 const scores = [0, 0];
@@ -20,8 +23,7 @@ let currentScore = 0;
 let activePlayer = 0;
 
 //Roll the dice
-const diceRoll = document.querySelector('.btn--roll');
-diceRoll.addEventListener('click', () => {
+btnRoll.addEventListener('click', () => {
   //Generate random number between 1 and 6
   let randomNumber = Math.ceil(Math.random() * 6);
 
@@ -35,9 +37,21 @@ diceRoll.addEventListener('click', () => {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0Element.classList.toggle('player--active');
-    player1Element.classList.toggle('player--active');
+    togglePlayer();
   }
 });
+
+//Hold functionality
+btnHold.addEventListener('click', () => {
+  scores[activePlayer] += currentScore;
+  scorePlayer0.textContent = scores[0];
+  scorePlayer1.textContent = scores[1];
+  togglePlayer();
+});
+
+const togglePlayer = () => {
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
